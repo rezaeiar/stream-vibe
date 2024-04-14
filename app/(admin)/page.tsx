e6924eb388f81c4a89b288cc95f25032b889790f
[rezaeiar] <rezaeiar0@gmail.com>
2024-04-14 13:45:42 +0430

    feat: Creating Devices and Categories components and developing Devices components

--- app/(admin)/page.tsx
+++ app/(admin)/page.tsx
@@ -1,9 +1,13 @@
 import Pricing from "../_components/modules/Pricing/Pricing";
+import Categories from "../_components/templates/Index/Categories/Categories";
+import Devices from "../_components/templates/Index/Devices/Devices";
 
 export default function Home() {
     return (
-        <main className="">
+        <>
+            {/* // <Categories /> */}
+            <Devices />
             <Pricing />
-        </main>
+        </>
     );
 }
---
+++ app/_components/templates/Index/Devices/Devices.tsx
@@ -0,0 +1,145 @@
+import React from 'react'
+import Image from 'next/image'
+
+export default function Devices() {
+    return (
+        <div>
+            <div className="container">
+                <div className="flex flex-col gap-10 lg:gap-14 2xl:gap-20">
+                    <div className="flex flex-col lg:flex-row gap-5">
+                        <div className="flex flex-col gap-2.5 2xl:gap-3.5">
+                            <h3 className='text-2xl lg:text-3xl 2xl:text-4xl font-manrope-bold text-white'>
+                                We Provide you streaming experience across various devices.
+                            </h3>
+                            <span className='text-sm lg:text-base 2xl:text-lg font-manrope-regular text-gray-60'>
+                                With StreamVibe, you can enjoy your favorite movies and TV shows anytime, anywhere.
+                            </span>
+                        </div>
+                    </div>
+                    <div className="grid grid-cols-1 lg:grid-cols-3 gap-5 2xl:gap-8">
+                        <div className="flex flex-col gap-5 lg:gap-6 2xl:gap-8 p-6 lg:p-10 2xl:p-12 rounded-lg border border-black-15 bg-gradient-to-bl from-red-45/20 via-black-6 to-black-6">
+                            <div className="flex items-center gap-2.5 lg:gap-3 2xl:gap-4">
+                                <div className="flex p-2.5 lg:p-3 2xl:p-4 rounded-md border border-black-15 bg-black-10">
+                                    <Image
+                                        src="/images/icons/smartphone.svg"
+                                        alt="smartphone"
+                                        width="100"
+                                        height="100"
+                                        sizes="100vw"
+                                        className="w-6 h-6 lg:w-8 lg:h-8 2xl:w-10 2xl:h-10"
+                                    />
+                                </div>
+                                <h3 className='text-white font-manrope-semibold text-lg lg:text-xl 2xl:text-2xl'>
+                                    Smartphones
+                                </h3>
+                            </div>
+                            <span className='text-gray-60 text-sm lg:text-base 2xl:text-lg font-manrope-regular'>
+                                StreamVibe is optimized for both Android and iOS smartphones. Download our app from the Google Play Store or the Apple App Store
+                            </span>
+                        </div>
+                        <div className="flex flex-col gap-5 lg:gap-6 2xl:gap-8 p-6 lg:p-10 2xl:p-12 rounded-lg border border-black-15 bg-gradient-to-bl from-red-45/20 via-black-6 to-black-6">
+                            <div className="flex items-center gap-2.5 lg:gap-3 2xl:gap-4">
+                                <div className="flex p-2.5 lg:p-3 2xl:p-4 rounded-md border border-black-15 bg-black-10">
+                                    <Image
+                                        src="/images/icons/smartphone.svg"
+                                        alt="smartphone"
+                                        width="100"
+                                        height="100"
+                                        sizes="100vw"
+                                        className="w-6 h-6 lg:w-8 lg:h-8 2xl:w-10 2xl:h-10"
+                                    />
+                                </div>
+                                <h3 className='text-white font-manrope-semibold text-lg lg:text-xl 2xl:text-2xl'>
+                                    Smartphones
+                                </h3>
+                            </div>
+                            <span className='text-gray-60 text-sm lg:text-base 2xl:text-lg font-manrope-regular'>
+                                StreamVibe is optimized for both Android and iOS smartphones. Download our app from the Google Play Store or the Apple App Store
+                            </span>
+                        </div>
+                        <div className="flex flex-col gap-5 lg:gap-6 2xl:gap-8 p-6 lg:p-10 2xl:p-12 rounded-lg border border-black-15 bg-gradient-to-bl from-red-45/20 via-black-6 to-black-6">
+                            <div className="flex items-center gap-2.5 lg:gap-3 2xl:gap-4">
+                                <div className="flex p-2.5 lg:p-3 2xl:p-4 rounded-md border border-black-15 bg-black-10">
+                                    <Image
+                                        src="/images/icons/smartphone.svg"
+                                        alt="smartphone"
+                                        width="100"
+                                        height="100"
+                                        sizes="100vw"
+                                        className="w-6 h-6 lg:w-8 lg:h-8 2xl:w-10 2xl:h-10"
+                                    />
+                                </div>
+                                <h3 className='text-white font-manrope-semibold text-lg lg:text-xl 2xl:text-2xl'>
+                                    Smartphones
+                                </h3>
+                            </div>
+                            <span className='text-gray-60 text-sm lg:text-base 2xl:text-lg font-manrope-regular'>
+                                StreamVibe is optimized for both Android and iOS smartphones. Download our app from the Google Play Store or the Apple App Store
+                            </span>
+                        </div>
+                        <div className="flex flex-col gap-5 lg:gap-6 2xl:gap-8 p-6 lg:p-10 2xl:p-12 rounded-lg border border-black-15 bg-gradient-to-bl from-red-45/20 via-black-6 to-black-6">
+                            <div className="flex items-center gap-2.5 lg:gap-3 2xl:gap-4">
+                                <div className="flex p-2.5 lg:p-3 2xl:p-4 rounded-md border border-black-15 bg-black-10">
+                                    <Image
+                                        src="/images/icons/smartphone.svg"
+                                        alt="smartphone"
+                                        width="100"
+                                        height="100"
+                                        sizes="100vw"
+                                        className="w-6 h-6 lg:w-8 lg:h-8 2xl:w-10 2xl:h-10"
+                                    />
+                                </div>
+                                <h3 className='text-white font-manrope-semibold text-lg lg:text-xl 2xl:text-2xl'>
+                                    Smartphones
+                                </h3>
+                            </div>
+                            <span className='text-gray-60 text-sm lg:text-base 2xl:text-lg font-manrope-regular'>
+                                StreamVibe is optimized for both Android and iOS smartphones. Download our app from the Google Play Store or the Apple App Store
+                            </span>
+                        </div>
+                        <div className="flex flex-col gap-5 lg:gap-6 2xl:gap-8 p-6 lg:p-10 2xl:p-12 rounded-lg border border-black-15 bg-gradient-to-bl from-red-45/20 via-black-6 to-black-6">
+                            <div className="flex items-center gap-2.5 lg:gap-3 2xl:gap-4">
+                                <div className="flex p-2.5 lg:p-3 2xl:p-4 rounded-md border border-black-15 bg-black-10">
+                                    <Image
+                                        src="/images/icons/smartphone.svg"
+                                        alt="smartphone"
+                                        width="100"
+                                        height="100"
+                                        sizes="100vw"
+                                        className="w-6 h-6 lg:w-8 lg:h-8 2xl:w-10 2xl:h-10"
+                                    />
+                                </div>
+                                <h3 className='text-white font-manrope-semibold text-lg lg:text-xl 2xl:text-2xl'>
+                                    Smartphones
+                                </h3>
+                            </div>
+                            <span className='text-gray-60 text-sm lg:text-base 2xl:text-lg font-manrope-regular'>
+                                StreamVibe is optimized for both Android and iOS smartphones. Download our app from the Google Play Store or the Apple App Store
+                            </span>
+                        </div>
+                        <div className="flex flex-col gap-5 lg:gap-6 2xl:gap-8 p-6 lg:p-10 2xl:p-12 rounded-lg border border-black-15 bg-gradient-to-bl from-red-45/20 via-black-6 to-black-6">
+                            <div className="flex items-center gap-2.5 lg:gap-3 2xl:gap-4">
+                                <div className="flex p-2.5 lg:p-3 2xl:p-4 rounded-md border border-black-15 bg-black-10">
+                                    <Image
+                                        src="/images/icons/smartphone.svg"
+                                        alt="smartphone"
+                                        width="100"
+                                        height="100"
+                                        sizes="100vw"
+                                        className="w-6 h-6 lg:w-8 lg:h-8 2xl:w-10 2xl:h-10"
+                                    />
+                                </div>
+                                <h3 className='text-white font-manrope-semibold text-lg lg:text-xl 2xl:text-2xl'>
+                                    Smartphones
+                                </h3>
+                            </div>
+                            <span className='text-gray-60 text-sm lg:text-base 2xl:text-lg font-manrope-regular'>
+                                StreamVibe is optimized for both Android and iOS smartphones. Download our app from the Google Play Store or the Apple App Store
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
