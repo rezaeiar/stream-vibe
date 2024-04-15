8f47ba0388f4b4b86b5e473fe61b4bc3b3c4d52c
[rezaeiar] <rezaeiar0@gmail.com>
2024-04-15 19:22:24 +0430

    feat: Creating a Categories component on the Index page

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -1,5 +1,6 @@
 import Header from "../_components/modules/Header/Header";
 import HeroSection from "../_components/templates/Index/HeroSection/HeroSection";
+import Categories from "../_components/templates/Index/Categories/Categories";
 import Devices from "../_components/templates/Index/Devices/Devices";
 import Pricing from "../_components/modules/Pricing/Pricing";
 
@@ -7,6 +8,7 @@ export default function Home() {
     return (
         <>
             <Header component={<HeroSection />} isNavigationBarFloat />
+            <Categories />
             <Devices />
             <Pricing />
         </>
