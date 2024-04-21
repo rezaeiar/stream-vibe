11c2b2623b95bd37d1c2f89bd2b5671c83be9860
[rezaeiar] <rezaeiar0@gmail.com>
2024-04-21 14:10:50 +0430

    fix: Rename the Features component to Plans

--- app/(pages)/subscription/page.tsx
+++ app/(pages)/subscription/page.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import Header from '@/app/_components/modules/Header/Header'
 import Pricing from '@/app/_components/modules/Pricing/Pricing'
-import Features from '@/app/_components/templates/Subscription/Features/Features'
+import Plans from '@/app/_components/templates/Subscription/Plans/Plans'
 
 export default function Subscription() {
     return (
         <>
             <Header />
             <Pricing />
-            <Features />
+            <Plans />
         </>
     )
 }
