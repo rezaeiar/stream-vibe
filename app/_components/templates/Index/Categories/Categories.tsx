a3ff1183007f79d4a56ce7ba7285e4281c410797
[rezaeiar] <rezaeiar0@gmail.com>
2024-04-25 11:30:40 +0430

    feat: Setting the light mode of the header of the Categories component

--- app/_components/templates/Index/Categories/Categories.tsx
+++ app/_components/templates/Index/Categories/Categories.tsx
@@ -8,26 +8,26 @@ export default function Categories() {
                 <div className="flex flex-col gap-10 lg:gap-14 2xl:gap-20">
                     <div className="flex flex-col lg:flex-row gap-5 justify-between">
                         <div className="flex flex-col gap-2.5 2xl:gap-3.5">
-                            <h3 className='text-2xl lg:text-3xl 2xl:text-4xl font-manrope-bold text-white'>
+                            <h3 className='text-2xl lg:text-3xl 2xl:text-4xl font-manrope-bold text-black-6 dark:text-white'>
                                 Explore our wide variety of categories
                             </h3>
-                            <span className='text-sm lg:text-base 2xl:text-lg font-manrope-regular text-gray-60'>
+                            <span className='text-sm lg:text-base 2xl:text-lg font-manrope-regular text-black-30 dark:text-gray-60'>
                                 Whether you're looking for a comedy to make you laugh, a drama to make you think, or a documentary to learn something new
                             </span>
                         </div>
-                        <div className="hidden lg:flex lg:gap-3 2xl:gap-4 items-center bg-black-6 lg:p-3 2xl:p-4 border border-black-12 lg:rounded-lg 2xl:rounded-xl">
-                            <button className="flex lg:p-2.5 2xl:p-3.5 rounded-md border border-black-12 text-white bg-black-10">
+                        <div className="hidden lg:flex lg:gap-3 2xl:gap-4 items-center bg-gray-99 dark:bg-black-6 lg:p-3 2xl:p-4 border border-gray-90 dark:border-black-12 lg:rounded-lg 2xl:rounded-xl">
+                            <button className="flex lg:p-2.5 2xl:p-3.5 rounded-md border border-gray-90 dark:border-black-12 text-black-6 dark:text-gray-99 bg-gray-95 dark:bg-black-10">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 2xl:h-7 2xl:w-7">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
                                 </svg>
                             </button>
                             <div className="flex gap-1">
-                                <div className="h-1 w-4 bg-black-20 rounded-full"></div>
+                                <div className="h-1 w-4 bg-gray-70 dark:bg-black-20 rounded-full"></div>
                                 <div className="h-1 w-5 bg-red-45 rounded-full"></div>
-                                <div className="h-1 w-4 bg-black-20 rounded-full"></div>
-                                <div className="h-1 w-4 bg-black-20 rounded-full"></div>
+                                <div className="h-1 w-4 bg-gray-70 dark:bg-black-20 rounded-full"></div>
+                                <div className="h-1 w-4 bg-gray-70 dark:bg-black-20 rounded-full"></div>
                             </div>
-                            <button className="flex lg:p-2.5 2xl:p-3.5 rounded-md border border-black-12 text-white bg-black-10">
+                            <button className="flex lg:p-2.5 2xl:p-3.5 rounded-md border border-gray-90 dark:border-black-12 text-black-6 dark:text-gray-99 bg-gray-95 dark:bg-black-10">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 2xl:h-7 2xl:w-7">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
                                 </svg>
