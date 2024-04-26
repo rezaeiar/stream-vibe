cda925a5a7ddf7b316cab305b7de3e4c0122afaa
[rezaeiar] <rezaeiar0@gmail.com>
2024-04-26 18:38:22 +0430

    feat: Create language change button in NavigationBar

--- app/_components/modules/ThemeSwitcher/ThemeSwitcher.tsx
+++ app/_components/modules/ThemeSwitcher/ThemeSwitcher.tsx
@@ -25,10 +25,10 @@ export default function ThemeSwitcher() {
     return (
         <div className="w-20 h-10 bg-gray-97 dark:bg-black-25 rounded-full border-2 border-gray-90 dark:border-black-12 relative cursor-pointer" onClick={changeMode}>
             <div className={`h-8 w-8 bg-gray-90 dark:bg-black-12 rounded-full absolute top-0.5 z-20 transition-all flex items-center justify-center ${theme === "dark" ? "right-0.5" : "right-[42px]"}`}>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-gray-75 hidden dark:block">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-gray-70 hidden dark:block">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
                 </svg>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-black-15 block dark:hidden">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-gray-60 block dark:hidden">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
                 </svg>
             </div>
