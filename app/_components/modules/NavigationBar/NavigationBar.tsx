d7900c14462975a8410342c79a7d03480b997cf1
[rezaeiar] <rezaeiar0@gmail.com>
2024-04-27 13:33:33 +0430

    feat: Add lightmode to the ContactForm component

--- app/_components/modules/NavigationBar/NavigationBar.tsx
+++ app/_components/modules/NavigationBar/NavigationBar.tsx
@@ -60,16 +60,11 @@ export default function NavigationBar({ float }: NavigationBarProps) {
                             </ul>
                         </div>
                         <div className="hidden lg:flex gap-3.5 items-center justify-between">
-                            <div className="flex h-10 w-10 rounded-full border-2 border-gray-90 dark:border-black-12 bg-gray-97 dark:bg-black-25 items-center justify-center">
+                            <div className="flex h-9 w-9 rounded-full border-2 border-gray-90 dark:border-black-12 bg-gray-97 dark:bg-black-25 items-center justify-center">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 xl:w-8 2xl:h-8 text-gray-60 dark:text-gray-70">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="m10.5 21 5.25-11.25L21 21m-9-3h7.5M3 5.621a48.474 48.474 0 0 1 6-.371m0 0c1.12 0 2.233.038 3.334.114M9 5.25V3m3.334 2.364C11.176 10.658 7.69 15.08 3 17.502m9.334-12.138c.896.061 1.785.147 2.666.257m-4.589 8.495a18.023 18.023 0 0 1-3.827-5.802" />
                                 </svg>
                             </div>
-                            {/* <div className="flex h-10 w-10 rounded-full border-2 border-gray-90 bg-gray-97 items-center justify-center">
-                                <span>
-                                    Fa
-                                </span>
-                            </div> */}
                             <ThemeSwitcher />
                         </div>
                     </div>
--- app/_components/templates/Support/ContactForm/ContactForm.tsx
+++ app/_components/templates/Support/ContactForm/ContactForm.tsx
@@ -8,14 +8,14 @@ export default function ContactForm() {
                 <div className="grid grid-cols-1 lg:grid-cols-3 gap-12 2xl:gap-20">
                     <div className="col-span-1 flex flex-col gap-8 lg:gap-10 2xl:gap-12">
                         <div className="flex flex-col gap-2.5 2xl:gap-3.5">
-                            <h3 className='font-manrope-bold text-3xl lg:text-4xl 2xl:text-5xl text-white'>
+                            <h3 className='font-manrope-bold text-3xl lg:text-4xl 2xl:text-5xl text-black-6 dark:text-gray-99'>
                                 Welcome to our support page!
                             </h3>
-                            <p className='font-manrope-regular text-sm lg:text-base 2xl:text-lg text-gray-60'>
+                            <p className='font-manrope-regular text-sm lg:text-base 2xl:text-lg text-black-30 dark:text-gray-60'>
                                 We're here to help you with any problems you may be having with our product.
                             </p>
                         </div>
-                        <div className="border-4 rounded-md aspect-square border-black-15">
+                        <div className="border-4 rounded-md aspect-square border-gray-75 dark:border-black-15">
                             <Image
                                 src="/images/banners/contact-form.png"
                                 alt="video-trailer"
@@ -26,45 +26,45 @@ export default function ContactForm() {
                             />
                         </div>
                     </div>
-                    <div className="col-span-1 lg:col-span-2 bg-black-6 border border-black-15 rounded-xl p-6 lg:p-10 2xl:p-12 grid grid-cols-1 lg:grid-cols-2 gap-x-5 2xl:gap-x-12 gap-y-5 lg:gap-y-10 2xl:gap-y-12 h-fit">
-                        <div className="flex flex-col gap-3 2xl:gap-4 font-manrope-semibold text-white">
+                    <div className="col-span-1 lg:col-span-2 bg-gray-99 dark:bg-black-6 border border-gray-75 dark:border-black-15 rounded-xl p-6 lg:p-10 2xl:p-12 grid grid-cols-1 lg:grid-cols-2 gap-x-5 2xl:gap-x-12 gap-y-5 lg:gap-y-10 2xl:gap-y-12 h-fit">
+                        <div className="flex flex-col gap-3 2xl:gap-4 font-manrope-semibold text-black-6 dark:text-gray-99">
                             <label htmlFor="" className="text-base 2xl:text-lg">
                                 First Name
                             </label>
-                            <input type="text" placeholder='Enter First Name' className='focus:border-red-51 outline-none p-4 2xl:p-5 text-sm 2xl:text-lg text-gray-60 font-manrope-regular bg-black-8 border border-black-15 rounded-md 2xl:rounded-lg' />
+                            <input type="text" placeholder='Enter First Name' className='focus:!border-red-51 outline-none p-4 2xl:p-5 text-sm 2xl:text-lg text-black-30 dark:text-gray-60 font-manrope-regular bg-gray-97 dark:bg-black-8 border border-gray-75 dark:border-black-15 rounded-md 2xl:rounded-lg' />
                         </div>
-                        <div className="flex flex-col gap-3 2xl:gap-4 font-manrope-semibold text-white">
+                        <div className="flex flex-col gap-3 2xl:gap-4 font-manrope-semibold text-black-6 dark:text-gray-99">
                             <label htmlFor="" className="text-base 2xl:text-lg">
                                 Last Name
                             </label>
-                            <input type="text" placeholder='Enter Last Name' className='focus:border-red-51 outline-none p-4 2xl:p-5 text-sm 2xl:text-lg text-gray-60 font-manrope-regular bg-black-8 border border-black-15 rounded-md 2xl:rounded-lg' />
+                            <input type="text" placeholder='Enter Last Name' className='focus:!border-red-51 outline-none p-4 2xl:p-5 text-sm 2xl:text-lg text-black-30 dark:text-gray-60 font-manrope-regular bg-gray-97 dark:bg-black-8 border border-gray-75 dark:border-black-15 rounded-md 2xl:rounded-lg' />
                         </div>
-                        <div className="flex flex-col gap-3 2xl:gap-4 font-manrope-semibold text-white">
+                        <div className="flex flex-col gap-3 2xl:gap-4 font-manrope-semibold text-black-6 dark:text-gray-99">
                             <label htmlFor="" className="text-base 2xl:text-lg">
                                 Email
                             </label>
-                            <input type="text" placeholder='Enter Email' className='focus:border-red-51 outline-none p-4 2xl:p-5 text-sm 2xl:text-lg text-gray-60 font-manrope-regular bg-black-8 border border-black-15 rounded-md 2xl:rounded-lg' />
+                            <input type="text" placeholder='Enter Email' className='focus:!border-red-51 outline-none p-4 2xl:p-5 text-sm 2xl:text-lg text-black-30 dark:text-gray-60 font-manrope-regular bg-gray-97 dark:bg-black-8 border border-gray-75 dark:border-black-15 rounded-md 2xl:rounded-lg' />
                         </div>
-                        <div className="flex flex-col gap-3 2xl:gap-4 font-manrope-semibold text-white">
+                        <div className="flex flex-col gap-3 2xl:gap-4 font-manrope-semibold text-black-6 dark:text-gray-99">
                             <label htmlFor="" className="text-base 2xl:text-lg">
                                 Phone Number
                             </label>
-                            <input type="text" placeholder='Enter Phone Number' className='focus:border-red-51 outline-none p-4 2xl:p-5 text-sm 2xl:text-lg text-gray-60 font-manrope-regular bg-black-8 border border-black-15 rounded-md 2xl:rounded-lg' />
+                            <input type="text" placeholder='Enter Phone Number' className='focus:!border-red-51 outline-none p-4 2xl:p-5 text-sm 2xl:text-lg text-black-30 dark:text-gray-60 font-manrope-regular bg-gray-97 dark:bg-black-8 border border-gray-75 dark:border-black-15 rounded-md 2xl:rounded-lg' />
                         </div>
-                        <div className="flex flex-col gap-3 2xl:gap-4 font-manrope-semibold text-white col-span-1 lg:col-span-2">
+                        <div className="flex flex-col gap-3 2xl:gap-4 font-manrope-semibold text-black-6 dark:text-gray-99 col-span-1 lg:col-span-2">
                             <label htmlFor="" className="text-base 2xl:text-lg">
                                 Message
                             </label>
-                            <textarea id="" className='focus:border-red-51 outline-none p-4 2xl:p-5 text-sm 2xl:text-lg text-gray-60 font-manrope-regular bg-black-8 border border-black-15 rounded-md 2xl:rounded-lg aspect-[5/1] resize-none' placeholder='Enter your Message'></textarea>
+                            <textarea id="" className='focus:!border-red-51 outline-none p-4 2xl:p-5 text-sm 2xl:text-lg text-black-30 dark:text-gray-60 font-manrope-regular bg-gray-97 dark:bg-black-8 border border-gray-75 dark:border-black-15 rounded-md 2xl:rounded-lg aspect-[5/1] resize-none' placeholder='Enter your Message'></textarea>
                         </div>
                         <div className="flex flex-col lg:flex-row gap-y-5 justify-between lg:items-center col-span-2">
-                            <div className="text-gray-60 font-manrope-regular text-base 2xl:text-lg flex gap-2 2xl:gap-2.5">
+                            <div className="text-black-30 dark:text-gray-60 font-manrope-regular text-base 2xl:text-lg flex gap-2 2xl:gap-2.5">
                                 <input type="checkbox" id="" />
                                 <span>
                                     I agree with Terms of Use and Privacy Policy
                                 </span>
                             </div>
-                            <button className='bg-red-45 text-sm 2xl:text-lg rounded-md py-3.5 2xl:py-4 px-5 text-white lg:w-fit'>
+                            <button className='bg-red-45 text-sm 2xl:text-lg rounded-md py-3.5 2xl:py-4 px-5 text-gray-99 lg:w-fit'>
                                 Send Message
                             </button>
                         </div>
