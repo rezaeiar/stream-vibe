6d5752c52827eaa279beced20a7ba3bb1589fd2f
[rezaeiar] <rezaeiar0@gmail.com>
2024-05-02 00:08:28 +0430

    feat: Create a logic to change the plan mode to monthly or yearly and add bilingual mode in Plans

--- app/_components/modules/Plans/Plans.tsx
+++ app/_components/modules/Plans/Plans.tsx
@@ -1,32 +1,35 @@
-import React from 'react'
-import Link from 'next/link'
+"use client"
+import { useTranslations } from 'next-intl'
 import PlanCard from '../PlanCard/PlanCard'
+import { useState } from 'react'
 
 export default function Plans() {
+    const t = useTranslations("plans")
+    const [subscriptionType, setSubscriptionType] = useState("monthly")
     return (
         <div id='plans'>
             <div className="container">
                 <div className="flex flex-col gap-10 lg:gap-14 2xl:gap-20">
                     <div className="flex flex-col lg:flex-row gap-5">
                         <div className="flex flex-col gap-2.5 2xl:gap-3.5">
-                            <h3 className='text-2xl lg:text-3xl 2xl:text-4xl font-manrope-bold text-black-6 dark:text-white'>
-                                Choose the plan that's right for you
+                            <h3 className='text-2xl lg:text-3xl 2xl:text-4xl font-manrope-bold rtl:font-vazir-bold text-black-6 dark:text-white'>
+                                {t("title")}
                             </h3>
-                            <span className='text-sm lg:text-base 2xl:text-lg font-manrope-regular text-black-30 dark:text-gray-60'>
-                                Join StreamVibe and select from our flexible subscription options tailored to suit your viewing preferences. Get ready for non-stop entertainment!
+                            <span className='text-sm lg:text-base 2xl:text-lg font-manrope-regular rtl:font-vazir-regular text-black-30 dark:text-gray-60'>
+                                {t("description")}
                             </span>
                         </div>
                         <div className="w-fit h-fit shrink-0 self-end border bg-gray-99 dark:bg-black-6 border-gray-90 dark:border-black-12 py-2 2xl:py-2.5 px-2 2xl:px-2.5 rounded-xl">
-                            <ul className="flex text-sm 2xl:text-lg gap-2 2xl:gap-2.5">
+                            <ul className="flex text-sm 2xl:text-lg gap-2 2xl:gap-2.5 font-manrope-medium rtl:font-vazir-medium">
                                 <li>
-                                    <Link href='/' className="block py-3 2xl:py-3.5 px-5 2xl:px-6 text-black-15 dark:text-gray-75 rounded-lg transition-colors hover:text-black-6 dark:hover:text-white hover:bg-gray-95 dark:hover:bg-black-10">
-                                        Monthly
-                                    </Link>
+                                    <button className={`block py-3 2xl:py-3.5 px-5 2xl:px-6 text-black-15 dark:text-gray-75 rounded-lg transition-colors hover:text-black-6 dark:hover:text-white hover:bg-gray-95 dark:hover:bg-black-10 ${subscriptionType === "monthly" ? "bg-gray-95 ": "dark:bg-black-10"}`} onClick={() => setSubscriptionType("monthly")}>
+                                        {t("Monthly")}
+                                    </button>
                                 </li>
                                 <li>
-                                    <Link href='/' className="block py-3 2xl:py-3.5 px-5 2xl:px-6 text-black-15 dark:text-gray-75 rounded-lg transition-colors hover:text-black-6 dark:hover:text-white hover:bg-gray-95 dark:hover:bg-black-10">
-                                        Yearly
-                                    </Link>
+                                    <button className={`block py-3 2xl:py-3.5 px-5 2xl:px-6 text-black-15 dark:text-gray-75 rounded-lg transition-colors hover:text-black-6 dark:hover:text-white hover:bg-gray-95 dark:hover:bg-black-10 ${subscriptionType === "yearly" ? "bg-gray-95 ": "dark:bg-black-10"}`} onClick={() => setSubscriptionType("yearly")}>
+                                        {t("Yearly")}
+                                    </button>
                                 </li>
                             </ul>
                         </div>
