0ca94f923bb5231e7689d017eb485793434773ec
[rezaeiar] <rezaeiar0@gmail.com>
2024-05-04 21:50:12 +0430

    feat: Bilingualizing the NavigationBar component

--- app/_components/modules/NavigationBar/NavigationBar.tsx
+++ app/_components/modules/NavigationBar/NavigationBar.tsx
@@ -2,10 +2,12 @@ import { NavigationBarProps } from "@/app/_types/components/NavigationBar/Naviga
 import Link from "next/link"
 import Image from "next/image"
 import ThemeSwitcher from "../ThemeSwitcher/ThemeSwitcher"
+import { useTranslations } from "next-intl"
 
 export default function NavigationBar({ float }: NavigationBarProps) {
+    const t = useTranslations("navigation-bar")
     return (
-        <div className={`py-3.5 lg:py-6 2xl:py-8 absolute top-0 w-full z-30 ${float ? 'absolute top-0' : "relative"}`}>
+        <div className={`py-3.5 lg:py-6 2xl:py-8 absolute top-0 w-full z-30 font-manrope-medium rtl:font-vazir-medium ${float ? 'absolute top-0' : "relative"}`}>
             <div className="container">
                 <div className="flex justify-between items-center">
                     <div className="w-full flex justify-between items-center">
@@ -39,28 +41,28 @@ export default function NavigationBar({ float }: NavigationBarProps) {
                             <ul className="flex text-sm 2xl:text-lg gap-2 2xl:gap-2.5">
                                 <li>
                                     <Link href='/' className="block py-3 2xl:py-3.5 px-5 2xl:px-6 text-black-15 dark:text-gray-75 rounded-lg transition-colors hover:text-black-6 dark:hover:text-gray-99 hover:bg-gray-95 dark:hover:bg-black-10">
-                                        Home
+                                        {t("Home")}
                                     </Link>
                                 </li>
                                 <li>
                                     <Link href='/movies' className="block py-3 2xl:py-3.5 px-5 2xl:px-6 text-black-15 dark:text-gray-75 rounded-lg transition-colors hover:text-black-6 dark:hover:text-gray-99 hover:bg-gray-95 dark:hover:bg-black-10">
-                                        Movies & Shows
+                                        {t("Movies & Shows")}
                                     </Link>
                                 </li>
                                 <li>
                                     <Link href='/support' className="block py-3 2xl:py-3.5 px-5 2xl:px-6 text-black-15 dark:text-gray-75 rounded-lg transition-colors hover:text-black-6 dark:hover:text-gray-99 hover:bg-gray-95 dark:hover:bg-black-10">
-                                        Support
+                                        {t("Support")}
                                     </Link>
                                 </li>
                                 <li>
                                     <Link href='/subscription' className="block py-3 2xl:py-3.5 px-5 2xl:px-6 text-black-15 dark:text-gray-75 rounded-lg transition-colors hover:text-black-6 dark:hover:text-gray-99 hover:bg-gray-95 dark:hover:bg-black-10">
-                                        Subscriptions
+                                        {t("Subscriptions")}
                                     </Link>
                                 </li>
                             </ul>
                         </div>
                         <div className="hidden lg:flex gap-3.5 items-center justify-between">
-                            <div className="flex h-9 w-9 rounded-full border-2 border-gray-90 dark:border-black-12 bg-gray-97 dark:bg-black-25 items-center justify-center">
+                            <div className="flex h-10 w-10 rounded-full border-2 border-gray-90 dark:border-black-12 bg-gray-97 dark:bg-black-25 items-center justify-center">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 xl:w-8 2xl:h-8 text-gray-60 dark:text-gray-70">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="m10.5 21 5.25-11.25L21 21m-9-3h7.5M3 5.621a48.474 48.474 0 0 1 6-.371m0 0c1.12 0 2.233.038 3.334.114M9 5.25V3m3.334 2.364C11.176 10.658 7.69 15.08 3 17.502m9.334-12.138c.896.061 1.785.147 2.666.257m-4.589 8.495a18.023 18.023 0 0 1-3.827-5.802" />
                                 </svg>
