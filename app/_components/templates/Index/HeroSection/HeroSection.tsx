4d8de92e6c38fa42f10bca7f748abf70e9a1bbcd
[rezaeiar] <rezaeiar0@gmail.com>
2024-05-05 01:26:23 +0430

    fix: Changing the appearance of the HeroSection component

--- app/_components/templates/Index/HeroSection/HeroSection.tsx
+++ app/_components/templates/Index/HeroSection/HeroSection.tsx
@@ -4,8 +4,8 @@ import Image from 'next/image'
 export default function HeroSection() {
     const t = useTranslations("hero-section")
     return (
-        <div className='relative bg-gray-90 dark:bg-transparent'>
-            <div className="absolute top-0 left-0 h-full w-full bg-gradient-to-b from-gray-99/10 dark:from-black-6/60 via-black-6/60 to-gray-97 dark:to-black-8/100 z-10"></div>
+        <div className='relative'>
+            <div className="absolute top-0 left-0 h-full w-full bg-gradient-to-b from-black-6/60 via-black-6/60 to-black-8/100 z-10 before:h-6 before:w-full before:bg-black-8 before:rounded-b-full before:absolute before:-bottom-6"></div>
             <Image
                 src="/images/banners/hero-section.png"
                 alt="video-trailer"
@@ -28,18 +28,18 @@ export default function HeroSection() {
                     </div>
                     <div className="flex flex-col justify-center items-center gap-8 lg:gap-12 2xl:gap-14 w-full">
                         <div className="flex flex-col gap-2.5 2xl:gap3.5">
-                            <h1 className='text-black-6 dark:text-gray-99 text-3xl lg:text-5xl 2xl:text-6xl font-manrope-bold rtl:font-vazir-bold text-center'>
+                            <h1 className='text-gray-99 text-3xl lg:text-5xl 2xl:text-6xl font-manrope-bold rtl:font-vazir-bold text-center'>
                                 {t("headline")}
                             </h1>
-                            <span className='block lg:hidden font-manrope-medium rtl:font-vazir-medium text-black-25 dark:text-gray-60 text-center text-sm'>
+                            <span className='block lg:hidden font-manrope-medium rtl:font-vazir-medium text-gray-60 text-center text-sm'>
                                 {t("description-mobile")}
                             </span>
-                            <span className='hidden lg:block font-manrope-medium rtl:font-vazir-medium text-black-25 dark:text-gray-60 text-center text-sm'>
+                            <span className='hidden lg:block font-manrope-medium rtl:font-vazir-medium text-gray-60 text-center text-sm'>
                                 {t("description-desktop")}
                             </span>
                         </div>
-                        <button className='bg-red-45 text-sm 2xl:text-lg rounded-md py-3.5 2xl:py-4 px-5 text-white w-fit'>
-                            Start Watching Now
+                        <button className='bg-red-45 text-sm 2xl:text-lg rounded-md font-manrope-medium rtl:font-vazir-medium py-3.5 2xl:py-4 px-5 text-white w-fit'>
+                           {t("button")}
                         </button>
                     </div>
                 </div>
