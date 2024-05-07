2f8d28e135e90ada30c5aa072a18a792ae5ef32e
[rezaeiar] <rezaeiar0@gmail.com>
2024-05-07 23:18:43 +0430

    feat: Start of slider development in the Categories component

--- app/_components/templates/Index/Categories/Categories.tsx
+++ app/_components/templates/Index/Categories/Categories.tsx
@@ -1,6 +1,10 @@
-import Image from 'next/image'
+"use client"
 import CategoryCard from '@/app/_components/modules/CategoryCard/CategoryCard'
-import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
 export default function Categories() {
     return (
@@ -17,7 +21,7 @@ export default function Categories() {
                             </span>
                         </div>
                         <div className="hidden lg:flex lg:gap-3 2xl:gap-4 items-center bg-gray-99 dark:bg-black-6 lg:p-3 2xl:p-4 border border-gray-90 dark:border-black-12 lg:rounded-lg 2xl:rounded-xl">
-                            <button className="flex lg:p-2.5 2xl:p-3.5 rounded-md border border-gray-90 dark:border-black-12 text-black-6 dark:text-gray-99 bg-gray-95 hover:bg-gray-97 dark:bg-black-10 dark:hover:bg-black-12 transition-colors">
+                            <button className="swiper-button-next flex lg:p-2.5 2xl:p-3.5 rounded-md border border-gray-90 dark:border-black-12 text-black-6 dark:text-gray-99 bg-gray-95 hover:bg-gray-97 dark:bg-black-10 dark:hover:bg-black-12 transition-colors">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 2xl:h-7 2xl:w-7">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
                                 </svg>
@@ -35,12 +39,33 @@ export default function Categories() {
                             </button>
                         </div>
                     </div>
-                    <div className="grid grid-cols-2 lg:grid-cols-5 gap-5 2xl:gap-8">
-                        <CategoryCard title='Action' />
-                        <CategoryCard title='Adventure' />
-                        <CategoryCard title='Comedy' />
-                        <CategoryCard title='Drama' />
-                        <CategoryCard title='Horror' />
+                    <div className="gap-5 2xl:gap-8 relative">
+                        <Swiper
+                            slidesPerView={4}
+                            spaceBetween={30}
+                            pagination={{
+                                clickable: true,
+                            }}
+                            navigation={true}
+                            modules={[Pagination, Navigation]}
+                            className="h-auto w-full relative"
+                        >
+                            <SwiperSlide className='relative bg-gray-95 dark:bg-black-10 border border-gray-75 dark:border-black-15 rounded-lg 2xl:rounded-xl p-5 lg:p-6 2xl:p-8 overflow-hidden'>
+                                <CategoryCard title='Action' />
+                            </SwiperSlide>
+                            <SwiperSlide className='relative bg-gray-95 dark:bg-black-10 border border-gray-75 dark:border-black-15 rounded-lg 2xl:rounded-xl p-5 lg:p-6 2xl:p-8 overflow-hidden'>
+                                <CategoryCard title='Adventure' />
+                            </SwiperSlide>
+                            <SwiperSlide className='relative bg-gray-95 dark:bg-black-10 border border-gray-75 dark:border-black-15 rounded-lg 2xl:rounded-xl p-5 lg:p-6 2xl:p-8 overflow-hidden'>
+                                <CategoryCard title='Comedy' />
+                            </SwiperSlide>
+                            <SwiperSlide className='relative bg-gray-95 dark:bg-black-10 border border-gray-75 dark:border-black-15 rounded-lg 2xl:rounded-xl p-5 lg:p-6 2xl:p-8 overflow-hidden'>
+                                <CategoryCard title='Drama' />
+                            </SwiperSlide>
+                            <SwiperSlide className='relative bg-gray-95 dark:bg-black-10 border border-gray-75 dark:border-black-15 rounded-lg 2xl:rounded-xl p-5 lg:p-6 2xl:p-8 overflow-hidden'>
+                                <CategoryCard title='Horror' />
+                            </SwiperSlide>
+                        </Swiper>
                     </div>
                 </div>
             </div>
